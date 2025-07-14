03644e423c5693f9c635090eaa6214026a53908a
Labeya <laurentmwn@gmail.com>
2025-07-14 20:59:47 +0100

    filter and sort year

--- resources/js/pages/admin/year/index.tsx
+++ resources/js/pages/admin/year/index.tsx
@@ -1,4 +1,5 @@
 import { Heading } from '@/components/heading';
+import { FilterInput } from '@/components/ui/filter-input';
 import { Pagination } from '@/components/ui/pagination';
 import AppLayout from '@/layouts/app-layout';
 import { YearsTable } from '@/shared/year/years-table';
@@ -20,7 +21,14 @@ export default function Index() {
             <div className="container-sidebar">
                 <Heading title="Années académiques">Consultez et gérez les différentes années académiques disponibles.</Heading>
                 <div className="mb-4 flex items-center justify-between">
-                    <div>{/* filtre à ajouter ici si besoin */}</div>
+                    <FilterInput
+                        availableSorts={[
+                            { label: 'Nom', value: 'name' },
+                            { label: 'Cloturée', value: 'is_closed' },
+                            { label: 'Mise à jour', value: 'updated_at' },
+                        ]}
+                        url={route('#year.index')}
+                    />
                 </div>
                 <YearsTable years={years.data} />
                 <Pagination items={years} />
